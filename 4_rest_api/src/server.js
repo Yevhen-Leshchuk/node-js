a7f9c73deaa2762c029fe0ff7db8566f290668e2
Yevhen-Leshchuk <evg.leschuk@gmail.com>
2023-04-05 23:32:10 +0300

    Add:methods (server.js).

---
+++ 4_rest_api/src/server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const path = require('path');
+const { getConfig } = require('./config');
+
+class UsersServer {
+  constructor() {
+    this.app = null;
+  }
+
+  start() {
+    this.initServer();
+    this.initConfig();
+    // this.initDatabase();
+    this.initMiddlewares();
+    this.initRoutes();
+    this.initErrorHandling();
+    this.startListening();
+  }
+
+  initServer() {
+    this.app = express();
+  }
+
+  initConfig() {
+    dotenv.config({ path: path.resolve(__dirname, '../.env') });
+  }
+
+  initMiddlewares() {
+    this.app.use(express.json({ limit: '500kb' }));
+  }
+
+  initRoutes() {}
+
+  initErrorHandling() {
+    this.app.use((err, req, res, next) => {
+      const statusCode = err.status || '500';
+      res.status(statusCode).send(err.message);
+    });
+  }
+
+  startListening() {
+    const { port } = getConfig();
+    this.app.listen(port, () => {
+      console.log('Started listening on port', port);
+    });
+  }
+}
+
+exports.UsersServer = UsersServer;
