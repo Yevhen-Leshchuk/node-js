f6883e2e3f2110c5f661527341be4dfc8b6104f0
Yevhen-Leshchuk <evg.leschuk@gmail.com>
2023-04-17 22:42:09 +0300

    Add:Cors config.

--- 4_rest_api/src/server.js
+++ 4_rest_api/src/server.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv');
 const path = require('path');
 const { getConfig } = require('./config');
 const { usersRouter } = require('./resources/users/users.controller');
+const morgan = require('morgan');
+const cors = require('cors');
 
 class UsersServer {
   constructor() {
@@ -29,6 +31,8 @@ class UsersServer {
 
   initMiddlewares() {
     this.app.use(express.json({ limit: '500kb' }));
+    this.app.use(morgan('tiny'));
+    this.configureCors();
   }
 
   initRoutes() {
@@ -47,6 +51,28 @@ class UsersServer {
     this.app.listen(port, () => {
       console.log('Started listening on port', port);
     });
+  }
+
+  configureCors() {
+    const { allowedCorsOrigin } = getConfig();
+
+    // this.app.use((req, res, next) => {
+    //   res.set('Access-Control-Allow-Origin', allowedCorsOrigin);
+    //   next();
+    // });
+    // this.app.options('/*', (req, res, next) => {
+    //   res.set(
+    //     'Access-Control-Allow-Methods',
+    //     req.get('Access-Control-Request-Method')
+    //   );
+    //   res.set(
+    //     'Access-Control-Allow-Headers',
+    //     req.get('Access-Control-Request-Headers')
+    //   );
+    //   return;
+    //   res.status(200).send();
+    // });
+    this.app.use(cors({ origin: allowedCorsOrigin }));
   }
 }
 
