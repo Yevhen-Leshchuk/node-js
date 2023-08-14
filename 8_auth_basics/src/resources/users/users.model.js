2572e389c30c27da1d91ea1ff91d697bc4689d91
Yevhen-Leshchuk <evg.leschuk@gmail.com>
2023-08-14 22:29:08 +0300

    Add:8_auth_basics.

---
+++ 8_auth_basics/src/resources/users/users.model.js
@@ -0,0 +1,12 @@
+const { required } = require('joi');
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  passwordHash: { type: String, required: true },
+});
+
+const UserModel = mongoose.model('User', userSchema);
+exports.UserModel = UserModel;
