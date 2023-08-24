2ae799407f4c163523a9414e6fd61cd1cb1ff7c3
Yevhen-Leshchuk <evg.leschuk@gmail.com>
2023-08-24 22:58:00 +0300

    Fix:authMiddleware (lesson-07).

--- lesson-07/src/middlewares/authMiddleware.js
+++ lesson-07/src/middlewares/authMiddleware.js
@@ -1,24 +1,24 @@
+/* eslint-disable spaced-comment */
 /* eslint-disable object-curly-spacing */
 const jwt = require('jsonwebtoken');
 const { NotAuthorizedError } = require('../helpers/errors');
 
 const authMiddleware = (req, res, next) => {
-  // eslint-disable-next-line no-unused-vars
-  const [typeToken, token] = req.headers['authorization'].split(' ');
-
-  if (!token) {
-    next(new NotAuthorizedError('Please provide a token'));
-  }
-
   try {
+    //TODO: validate tape token later (typeToken).
+    const [token] = req.headers['authorization'].split(' ');
+
+    if (!token) {
+      next(new NotAuthorizedError('Please provide a token'));
+    }
+
     const user = jwt.decode(token, process.env.JWT_SECRET);
     req.token = token;
     req.user = user;
+    next();
   } catch (error) {
     next(new NotAuthorizedError('Invalid a token'));
   }
-
-  next();
 };
 
 module.exports = {
