05f1f0e8863d77ae0e9e5847250d2c6806c3e6fe
Yevhen-Leshchuk <evg.leschuk@gmail.com>
2023-09-15 15:39:10 +0300

    Add:test for sum.js (10_testing).

--- 10_testing/sum.js
+++ 10_testing/sum.js
@@ -1,3 +1,9 @@
 exports.sum = (...args) => {
-  return args.reduce((sum, currentValue) => sum + currentValue, 0);
+  return args.reduce((sum, currentValue) => round(sum + currentValue, 5), 0);
 };
+
+function round(value, mantissaPoint) {
+  const discharge = Math.pow(10, mantissaPoint);
+
+  return Math.round(value * discharge) / discharge;
+}
