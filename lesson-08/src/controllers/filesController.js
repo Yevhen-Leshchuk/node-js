828df8560d406bec63e2b736f8c214241b0083fb
Yevhen-Leshchuk <evg.leschuk@gmail.com>
2023-09-20 17:32:15 +0300

    Update:lesson-08.

--- lesson-08/src/controllers/filesController.js
+++ lesson-08/src/controllers/filesController.js
@@ -1,11 +1,5 @@
 /* eslint-disable object-curly-spacing */
-const { upload } = require('../services/filesService');
-
 const uploadController = async (req, res) => {
-  const { email, password } = req.body;
-
-  await registration(email, password);
-
   res.json({ status: 'success' });
 };
 
